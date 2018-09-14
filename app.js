cfa595b50a85274e0fee991a5c5609119d8c84a1
ColeAmodeo <camodeo1@live.ca>
2018-09-14 17:40:54 -0400

    timer working, interval is auto starting

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
+
 moment().format();
+// Creating environment for timer function
+var start;
+var startDate;
+var end;
+var duration;
+var minutes = 0;
+var hours = 0;
+var secondsPassed = 0;
+function timer() {
+  secondsPassed = parseInt(secondsPassed + 1);
+  console.log(secondsPassed)
+  // Time calculations for hours, minutes and seconds
+  if (secondsPassed === 60) {
+    secondsPassed = 0;
+    minutes++;
+  }
+  if (minutes === 60) {
+    minutes = 0;
+    hours++;
+  }
+  // Display the result in the element with id="display"
+  $("#display").text(hours + "h " + minutes + "m " + secondsPassed + "s ")
+};
+//Start button
+function startSession () {
+  clearInterval(timer)
+  start = new moment()
+  console.log(start)
+  setInterval(timer, 1000)
+};
+//End Button
+function endSession () {
+ end = new moment()
+ console.log(end)
+ duration = moment.duration(end.diff(start))
+ console.log(duration)
+};
 
 $("#start-btn").click(startSession())
 $("#end-btn").click(endSession())
-
-// Creating environment for timer function
-var x;
-var y;
-var duration;
-// On start button, grab a beginning value of time.
-// On end session button, grab an ending value of time.
-// Send this duration to the database relative to the staff ID
-function startSession () {
- startObject = new moment()
- console.log(x)
- runTime()
-}
-
-function endSession () {
- endObject = new moment()
- console.log(y)
- duration = moment.duration(y.diff(x))
- console.log(duration)
-}
-
-var runTime = setInterval(function() {
-  //Set running timer
-  var activeTime = 0
-  activeTime++;
-  console.log(activeTime)
-  // Time calculations for hours, minutes and seconds
-  var hours = Math.floor((activeTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((activeTime % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((activeTime % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="display"
-  $("#display").text(hours + "h " + minutes + "m " + seconds + "s ")
-}, 1000);
